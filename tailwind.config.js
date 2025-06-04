f517a3a7dcb8ada46c32289124aeaa38f230cc94
Martinekong <martinekongsrud@outlook.com>
2025-06-04 15:27:01 +0200

    added custom typography

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,7 +3,11 @@
 module.exports = {
   content: ['./**/*.{html,js}', '!./node_modules/**/*'],
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+        custom: ['Urbanist', 'sans-serif'],
+      },
+    },
   },
   darkMode: 'class',
   plugins: [],
