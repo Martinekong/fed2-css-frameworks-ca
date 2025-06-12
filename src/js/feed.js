d3971fe0f841660ca49e11f2ebf67d38d181ef0d
Martinekong <martinekongsrud@outlook.com>
2025-06-12 15:30:21 +0200

    added create-post overlay

--- src/js/feed.js
+++ src/js/feed.js
@@ -2,6 +2,9 @@ const newestBtn = document.getElementById('newest');
 const oldestBtn = document.getElementById('oldest');
 const feedContainer = document.getElementById('feed-container');
 const cards = Array.from(feedContainer.children);
+const createPostBtn = document.getElementById('create-post-btn');
+const createPostModal = document.getElementById('create-post-modal');
+const postBtn = document.getElementById('post-btn');
 
 let isActive = true;
 
@@ -20,5 +23,22 @@ oldestBtn.addEventListener('click', () => {
     oldestBtn.classList.add('border-b-2');
     cards.reverse().forEach((card) => feedContainer.appendChild(card));
     isActive = false;
+  }
+});
+
+createPostBtn.addEventListener('click', () => {
+  createPostModal.classList.remove('hidden');
+  createPostModal.classList.add('flex');
+});
+
+postBtn.addEventListener('click', () => {
+  createPostModal.classList.remove('flex');
+  createPostModal.classList.add('hidden');
+});
+
+createPostModal.addEventListener('click', (event) => {
+  if (event.target === createPostModal) {
+    createPostModal.classList.remove('flex');
+    createPostModal.classList.add('hidden');
   }
 });
